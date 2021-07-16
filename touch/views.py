908017f003487d560583b99ef7b995c106a013c3
itzomen <peng.akebuon2468@gmail.com>
2021-07-16 21:06:02 +0100

    created views and urls.py

--- touch/views.py
+++ touch/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+
+    # get user input from request
+    input = request.GET.get("input")
+    output = ""
+
+    if input == "quit":
+        output = "Goodbye, You Just Quited"
+
+    context = {"input": input, "output": output}
+    return render(request, "index.html", context)
